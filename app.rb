bb44d5ceda05971e6fd7871589e0f961d5dbf0fe
Marla Glasser <mbglasser@gmail.com>
2018-05-01 11:03:19 -0400

    Add win/lose handling.

--- app.rb
+++ app.rb
@@ -54,18 +54,24 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    status = @game.check_win_or_lose 
+    if status == :win 
+      erb :win 
+    elsif status == :lose
+      erb :lose
+    else
+      erb :show
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    erb :show # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    erb :show # You may change/remove this line
   end
   
 end
