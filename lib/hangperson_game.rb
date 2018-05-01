5b671fda2974775912e25aa40e67b5de38b6c29f
Marla Glasser <mbglasser@gmail.com>
2018-05-01 09:51:28 -0400

    Finalize hangperson_game

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -9,8 +9,59 @@ class HangpersonGame
   # end
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  # Player guesses a letter
+  def guess(letter)
+    # Error Handling
+    if letter == nil || letter.class != String || 
+       letter == '' || 
+       letter =~ /[^A-Za-z]/ || 
+       letter.length != 1
+      raise ArgumentError, "Guess must be a letter"
+    # Already guessed letter
+    elsif @guesses.include?(letter.downcase) || @wrong_guesses.include?(letter.downcase)
+      return false
+    # Found a match -> add to @guesses
+    elsif word.include?(letter.downcase)
+      @guesses += letter 
+    # No match -> add to @wrong_guesses
+    else
+      @wrong_guesses += letter 
+    end
+  end
+
+  def word_with_guesses
+    # Set up toDisplay as "----" matching length of word
+    toDisplay = ""
+    @word.chars do |letter|
+      if @guesses.include? letter
+        toDisplay += letter 
+      else
+        toDisplay += '-'
+      end
+    end
+    toDisplay
+  end
+
+  def check_win_or_lose
+    # 7 incorrect guesses -> lose
+    if @wrong_guesses.length >= 7
+      return :lose
+    # Display word has no '-' -> guessed all letters -> win
+    elsif !word_with_guesses.include? '-'
+      return :win
+    else
+      return :play
+    end
+  end
+
+
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
